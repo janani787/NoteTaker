6e74cc8bf1e44bd118306c19e2bbb56291698d70
janani787 <evergreen.787@gmail.com>
2024-06-10 15:19:52 -0500

    save

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+
+// Import Express.js
+const express = require('express');
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require('path');
+
+// Initialize an instance of Express.js
+const app = express();
+
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
+// Static middleware pointing to the public folder
+app.use(express.static('public'));
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
+
+// listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
